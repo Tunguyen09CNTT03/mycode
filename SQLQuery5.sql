14d01b1b6a2115b0487c421378840054eed0d168
Tunguyen09CNTT03 <118881853+Tunguyen09CNTT03@users.noreply.github.com>
2022-11-29 07:58:07 +0700

    Add files via upload

--- SQLQuery5.sql
+++ SQLQuery5.sql
@@ -1,19 +1,8 @@
+﻿
+select *from Xuat;
 
-select *from PHANCONG;
-select *from DEAN;
-
-declare @Thongke table (MaDa int, Thoigian float)
-insert into @Thongke 
-	select MADA, sum(thoigian) as 'Tong thoi gian'
-	from PHANCONG 
-	group by MADA;
-
-select TENDEAN,cast(Thoigian as varchar (10)) as 'Tong thoi gian ' 
-	from @Thongke a  inner join DEAN b on a.MaDa=b.MADA
-select TENDEAN,convert (varchar (10),Thoigian) as 'Tong thoi gian ' 
-	from @Thongke a  inner join DEAN b on a.MaDa=b.MADA
-
-select TENDEAN,cast(Thoigian as decimal (5,2)) as 'Tong thoi gian ' 
-	from @Thongke a  inner join DEAN b on a.MaDa=b.MADA
-select TENDEAN,convert (decimal (5,2),Thoigian) as 'Tong thoi gian ' 
-	from @Thongke a  inner join DEAN b on a.MaDa=b.MADA
+declare @TienBan table (TienBan int)
+insert into @TienBan
+	 select TienBan,SoLuong
+	where PHG in (select MAPHG from PHONGBAN where TENPHG=N'Nghiên cứu '));
+	from @TienBan
